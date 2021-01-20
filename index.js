31687e7a3cdf1a22be8bedd5c1f79a6ded4cb79f
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-19 23:54:16 -0600

    commit tree 05593df51c9bddbdb4a1156ac586ee1bbbe5c9b8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map (array, callBack){
+    let arr = []
+    array.forEach(elem => arr.push(callBack(elem)))
+    return arr
+}
+
+function reduce(array, func, startingPoint){
+    
+    let next;
+    let start;
+    if (startingPoint){
+        next = startingPoint
+        start = 0
+    } else {
+        next = array[0]
+        start = 1
+    }
+    for (let i=start; i < array.length;i++){
+        next = func(next, array[i])
+    }
+    return next 
+} 
+
+
